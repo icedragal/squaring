737bc2d0886d236a45eeb20b7a32c81fb6f211bc
Icedragal <icedragal@gmail.com>
2022-11-28 19:16:44 +0300

    Билет 7, задача: написать метод, который возводит число в квадрат

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
-public class Main {
+class Main {
+
     public static void main(String[] args) {
 
+        System.out.println(squaring(5));
+
     }
+
+    public static int squaring(int value) {
+
+        return value*value;
+
+    }
+
 }
